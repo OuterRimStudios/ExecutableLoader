ef176fe28e7b71e53819271865e5bf1146d48ddc
hectorborges <38707692+hectorborges@users.noreply.github.com>
2019-10-03 14:48:37 -0500

    added basic docking area

---
+++ Assets/Scripts/DockManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DockManager : MonoBehaviour
+{
+    public static DockManager Instance;
+    public Dock[] testers;
+    public GameObject projectTemplate;
+
+    public GameObject environment;
+
+    public AudioClip hover;
+    public AudioClip selected;
+    public AudioSource source;
+
+    bool played;
+    bool launched;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        foreach(Dock tester in testers)
+        {
+            Project project = Instantiate(projectTemplate, transform).GetComponent<Project>();
+            project.SetUpProject(tester.name, tester.sky);
+        }
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        launched = false;
+        played = false;
+    }
+
+    public void Interacting()
+    {
+        environment.SetActive(false);
+
+        if(!played)
+        {
+            played = true;
+            source.PlayOneShot(hover);
+        }
+    }
+
+    public void StoppedInteracting()
+    {
+        environment.SetActive(true);
+        played = false;
+    }
+
+    public void Selected()
+    {
+        if(!launched)
+        {
+            launched = true;
+            source.PlayOneShot(selected);
+        }
+    }
+}
+
+[System.Serializable]
+public class Dock
+{
+    public string name;
+    public Material sky;
+}
---
+++ Assets/Scripts/Project.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Project : MonoBehaviour
+{
+    string projectName;
+
+    Material defaultSky;
+    Material skyboxMaterial;
+
+    TextMeshProUGUI text;
+
+    void Start()
+    {
+        defaultSky = RenderSettings.skybox;
+    }
+
+    public void SetUpProject(string _name, Material _sky)
+    {
+        projectName = _name;
+        skyboxMaterial = _sky;
+        text = GetComponentInChildren<TextMeshProUGUI>();
+        text.text = projectName;
+    }
+
+    public void Select()
+    {
+
+    }
+
+    public void Interact()
+    {
+        RenderSettings.skybox = skyboxMaterial;
+    }
+
+    public void StopInteraction()
+    {
+        RenderSettings.skybox = defaultSky;
+    }
+}
